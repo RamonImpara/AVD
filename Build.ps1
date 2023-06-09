e5ff2a04d80c7c8633e8c1acd22ae6fda38dff56
RamonImpara <133757581+RamonImpara@users.noreply.github.com>
2023-06-09 13:31:21 +0200

    Add files via upload

---
+++ Build.ps1
@@ -0,0 +1,50 @@
+﻿# Step 1: Import module
+Import-Module Az.Accounts
+
+# Step 2: get existing context
+$currentAzContext = Get-AzContext
+
+# Destination image resource group
+$imageResourceGroup="RG-AVDImageBuilder"
+
+# Location (see possible locations in the main docs)
+$location="westeurope"
+
+# Your subscription. This command gets your current subscription
+$subscriptionID=$currentAzContext.Subscription.Id
+
+# Image template name
+$imageTemplateName="AVD-WIN10ImageTemplate"
+
+# Distribution properties object name (runOutput). Gives you the properties of the managed image on completion
+$runOutputName="sigOutput"
+
+# setup role def names, these need to be unique
+$imageRoleDefName="Azure Image Builder Image Def"
+$identityName="AVD-AIB-Identity"
+
+$identityNameResourceId=$(Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName).Id
+$identityNamePrincipalId=$(Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $identityName).PrincipalId
+
+$aibRoleImageCreationUrl="https://raw.githubusercontent.com/RamonImpara/AVD/main/aibRoleImageCreation.json"
+$aibRoleImageCreationPath = "aibRoleImageCreation.json"
+
+$sigGalleryName= "AVD_AIB_Gallery"
+$imageDefName ="win10avd"
+
+$templateUrl="https://raw.githubusercontent.com/RamonImpara/AVD/main/armTemplateWVD.json"
+$templateFilePath = "c:\temp\armTemplateWVD.json"
+
+Invoke-WebRequest -Uri $templateUrl -OutFile $templateFilePath -UseBasicParsing
+
+((Get-Content -path $templateFilePath -Raw) -replace '<subscriptionID>',$subscriptionID) | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<rgName>',$imageResourceGroup) | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<region>',$location) | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<runOutputName>',$runOutputName) | Set-Content -Path $templateFilePath
+
+((Get-Content -path $templateFilePath -Raw) -replace '<imageDefName>',$imageDefName) | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<sharedImageGalName>',$sigGalleryName) | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<region1>',$location) | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<imgBuilderId>',$identityNameResourceId) | Set-Content -Path $templateFilePath
+
+New-AzResourceGroupDeployment -ResourceGroupName $imageResourceGroup -TemplateFile $templateFilePath -TemplateParameterObject @{"api-Version" = "2020-02-14"} -imageTemplateName $imageTemplateName -svclocation $location
